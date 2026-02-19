7777451b84813bab7ac88f9cc30d6816b0f6eb84
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-02-19 11:24:51 -0300

    ADD: Coleta de elementos e função para mostrar resultado.

---
+++ calculo-imc.js
@@ -0,0 +1,9 @@
+const inputPeso = document.getElementById("weight");
+const inputAltura = document.getElementById("height");
+const textoResultado = document.getElementById("resultText");
+const formulario = document.getElementById("imcForm");
+
+function mostrarResultado(mensagem){
+    textoResultado.textContent.Content = mensagem;
+}
+
