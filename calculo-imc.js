7a9ce993ec8c83755f1e0037ae11ec20e4fcb6e0
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-02-20 13:44:24 -0300

    Arquivos 1 e 2 de js completo

--- calculo-imc.js
+++
@@ -1,9 +0,0 @@
-const inputPeso = document.getElementById("weight");
-const inputAltura = document.getElementById("height");
-const textoResultado = document.getElementById("resultText");
-const formulario = document.getElementById("imcForm");
-
-function mostrarResultado(mensagem){
-    textoResultado.textContent.Content = mensagem;
-}
-
